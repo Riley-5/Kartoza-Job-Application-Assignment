25b809c990f35a50cb068b5ff7c74de0e59b97c7
Riley <riley.kamstra@gmail.com>
2022-08-20 11:29:52 +0200

    Created the extended user class

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,5 +1,22 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    home_address = models.Charfield(max_length=500)
+    phone_number = models.PositiveBigIntegerField()
+    location_latitude = models.DecimalField(max_digits=30, decimal_places=28)
+    location_longtitude = models.DecimalField(max_digits=30, decimal_places=28)
+
+    def __str__(self):
+        return f"{self.id} | {self.username}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "home_address": self.home_address,
+            "phone_number": self.phone_number,
+            "location_latitude": self.location_latitude,
+            "location_longitude": self.location_longtitude
+        }
