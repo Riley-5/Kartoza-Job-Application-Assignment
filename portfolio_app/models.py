08994a40a599898cb9268feea4d822f20cd1ebdf
Riley <riley.kamstra@gmail.com>
2022-08-20 12:06:42 +0200

    Created an unstyled user profile page

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    home_address = models.Charfield(max_length=500)
-    phone_number = models.PositiveBigIntegerField()
-    location_latitude = models.DecimalField(max_digits=30, decimal_places=28)
-    location_longtitude = models.DecimalField(max_digits=30, decimal_places=28)
+    home_address = models.CharField(max_length=500, null=True)
+    phone_number = models.PositiveBigIntegerField(null=True)
+    location_latitude = models.DecimalField(max_digits=30, decimal_places=28, null=True)
+    location_longtitude = models.DecimalField(max_digits=30, decimal_places=28, null=True)
 
     def __str__(self):
         return f"{self.id} | {self.username}"
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    template = loader.get_template("portfolio_app/index.html")
+    context = {
+        "user": request.user
+    }
+    return HttpResponse(template.render(context, request))
